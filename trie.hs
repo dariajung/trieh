634bc6a82b642051bf2356383dd23ec1d22af61f
Daria Jung <djj2115cu@gmail.com>
2014-07-10 13:10:08 -0400

    create emptyTrie func, update value to designate end of word, start working on insertion

--- trie.hs
+++ trie.hs
@@ -1,16 +1,34 @@
 module Trie where
 
 import qualified Data.Map as Map
+import Control.Monad
 
-{-- 
-    The Trie data type.
+{- 
+    The Node data type.
+    
     The value can be optional, hence the Maybe data type.
-    Children of a Trie are mapped Char to Trie.
-    The fullWord value denotes if the Trie holds a value that
-    marks the end of a valid word.
---}
-data Trie a = Trie {
+    Value is also a tuple of the current value and a bool
+    to designate if the current value marks the end of a
+    word. 
+
+    Children of a Node are mapped Char to Node.
+-}
+data Node a = Node {
     value :: Maybe a,
-    children :: Map.Map Char (Trie a),
-    fullWord :: Bool
+    children :: Map.Map Char (Node a)
 }
+
+type Trie = Node (String, Bool)
+
+updateEndWord :: Maybe (String, Bool) -> Maybe (String, Bool)
+updateEndWord = liftM update
+    where update (v, _) = (v, True)
+
+emptyTrie :: Trie
+emptyTrie = Node {
+    value = Nothing,
+    children = Map.empty
+}
+
+insert :: String -> Trie -> Trie
+insert [] trie = trie { value = updateEndWord $ value trie }
